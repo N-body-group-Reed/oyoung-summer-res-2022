1b17baa876d81ccf18c8727e73be6e7fb83d1798
NoahUngerSchulz <58535323+NoahUngerSchulz@users.noreply.github.com>
2022-07-21 13:46:40 -0700

    added energy

--- NSim.h
+++ NSim.h
@@ -11,6 +11,7 @@
 #include "PoissonSolver.h"
 #include "Integrator.h"
 
+int Energy(std::vector<class Particle>& ps);
 void NSim_Step(std::vector<class Particle>& ps, class Tree* T, double dt);
 void NSim_Init(std::vector<class Particle>& ps, int num_ps);
 
