c9d2141a3200716673337ca1e1a99ef64c848826
NoahUngerSchulz <58535323+NoahUngerSchulz@users.noreply.github.com>
2022-07-25 09:27:15 -0700

    Update NSim.cpp

--- NSim.cpp
+++ NSim.cpp
@@ -22,11 +22,46 @@ int Energy(std::vector<class Particle>& ps) {
   }
   return TE;
 }
+std::valarray<double> momentum(std::vector<class Particle>& ps) {
+  std::valarray<double> TP = {0.0,0.0,0.0};
+  for (std::vector<class Particle>::iterator p = ps.begin(); p != ps.end(); p++) {
+    std::valarray<double> v = p->vel;
+    std::valarray<double> P =v * p->m;
+  TP+=P;
+  }
+  return TP;
+}
 
+std::valarray<double> spin(std::vector<class Particle>& ps) {
+  std::valarray<double> TP = {0.0,0.0,0.0};
+  std::valarray<double> com = {0.0,0.0,0.0};
+  double totalmass=0;
+  for (std::vector<class Particle>::iterator p = ps.begin(); p != ps.end(); p++) {
+    totalmass+=p->m;
+    com+=p->pos*p->m;
+  }
+  com=com/totalmass;
+  for (std::vector<class Particle>::iterator p = ps.begin(); p != ps.end(); p++) {
+    std::valarray<double> v = p->vel;
+    std::valarray<double> r =p->pos-com;
+    std::valarray<double> P ={v[2]*r[3]-v[3]*r[2],   v[3]*r[1]-v[1]*r[3],   v[1]*r[2]-v[2]*r[1]};
+    P=p->m*P;
+  TP+=P;
+  }
+  return TP;
+}
+double mag(std::valarray<double> P){
+  double mag;
+  for (int i=0; i != 3;i++) mag+=pow(P[i],2);
+  mag=pow(mag,0.5);
+  return mag;
+}
 
 void NSim_Step(std::vector<class Particle>& ps, class Tree* T, double dt) {
 	Integrator(ps, T, dt);
 	if (T != nullptr) std::cout<<(Energy(ps)-EnergyInit)/EnergyInit<<std::endl;
+	std::cout<<"momentum "<<mag(momentum(ps)-momentumInit)/mag(momentumInit)<<std::endl;
+	std::cout<<"spin "<<mag(spin(ps)-spinInit)/mag(spinInit)<<std::endl;
 }
 
 void NSim_Init(std::vector<class Particle>& ps, int num_ps) {
